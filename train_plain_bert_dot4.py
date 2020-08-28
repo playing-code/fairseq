70cc743835ffed25bb6b777f6f5508b7e1005cac
RUCer <lsqrucer@gmail.com>
2020-08-28 04:22:53 +0000

    **

--- train_plain_bert_dot4.py
+++ train_plain_bert_dot4.py
@@ -47,6 +47,22 @@ lr=1e-4
 T_warm=5000
 all_iteration=33040
 
+# def init_process(rank,local_rank,args,minutes=720):
+#     """ Initialize the distributed environment. """
+#     # os.environ['MASTER_ADDR'] = '127.0.0.1'
+#     # os.environ['MASTER_PORT'] = '1234'
+#     # torch.distributed.init_process_group(backend, rank=rank, world_size=size)
+#     dist_init_method = 'tcp://{master_ip}:{master_port}'.format(
+#         master_ip='localhost', master_port='12345')
+#     dist.init_process_group(backend='nccl',
+#                 init_method=dist_init_method,
+#                 # If you have a larger dataset, you will need to increase it.
+#                 timeout=timedelta(minutes=minutes),
+#                 world_size=args.size,
+#                 rank=rank)
+#     num_gpus = torch.cuda.device_count()
+#     torch.cuda.set_device(local_rank)
+#     assert torch.distributed.is_initialized()
 
 def parse_args():
     parser = argparse.ArgumentParser("Transformer-XH")
@@ -60,10 +76,20 @@ def parse_args():
     parser.add_argument("--data_file",
                     type=str,
                     help="local_rank for distributed training on gpus")
+    parser.add_argument("--test_data_file",
+                    type=str,
+                    help="local_rank for distributed training on gpus")
     parser.add_argument("--feature_file",
                     type=str,
                     help="local_rank for distributed training on gpus")
+    parser.add_argument("--test_feature_file",
+                    type=str,
+                    help="local_rank for distributed training on gpus")
     parser.add_argument("--size",
+                    type=int,
+                    default=1,
+                    help="local_rank for distributed training on gpus")
+    parser.add_argument("--gpu_size",
                     type=int,
                     default=1,
                     help="local_rank for distributed training on gpus")
@@ -72,6 +98,9 @@ def parse_args():
                     default=1,
                     help="local_rank for distributed training on gpus")
     parser.add_argument("--log_file",
+                    type=str,
+                    help="local_rank for distributed training on gpus")
+    parser.add_argument("--field",
                     type=str,
                     help="local_rank for distributed training on gpus")
 
@@ -109,11 +138,10 @@ def group_labels_func(labels, preds, group_keys):
     all_keys = list(set(group_keys))
     group_labels = {k: [] for k in all_keys}
     group_preds = {k: [] for k in all_keys}
-
+    
     for l, p, k in zip(labels, preds, group_keys):
         group_labels[k].append(l)
         group_preds[k].append(p)
-
     all_labels = []
     all_preds = []
     for k in all_keys:
@@ -122,22 +150,57 @@ def group_labels_func(labels, preds, group_keys):
 
     return all_labels, all_preds
 
+
+
+def test(model,arges):
+    preds = []
+    labels = []
+    imp_indexes = []
+    metrics=['group_auc']
+    test_file=os.path.join(args.data_dir, args.test_data_file)  
+    preds = []
+    labels = []
+    imp_indexes = []
+    feature_file=os.path.join(args.data_dir,args.feature_file)
+    iterator=NewsIterator(batch_size=900, npratio=-1,feature_file=feature_file,field=args.field)
+    print('test...')
+    with torch.no_grad():
+        data_batch=iterator.load_data_from_file(test_file)
+        batch_t=0
+        for  imp_index , user_index, his_id, candidate_id , label  in data_batch:
+            batch_t+=len(candidate_id)
+            his_id=his_id.cuda(cudaid)
+            candidate_id= candidate_id.cuda(cudaid)
+            logit=model(his_id,candidate_id,None,mode='validation')
+            # print('???',label_t,label)
+            # assert 1==0
+            logit=list(np.reshape(np.array(logit.cpu()), -1))
+            label=list(np.reshape(np.array(label), -1))
+            imp_index=list(np.reshape(np.array(imp_index), -1))
+
+            labels.extend(label)
+            preds.extend(logit)
+            imp_indexes.extend(imp_index)
+            print('all data: ',len(labels))
+
+    group_labels, group_preds = group_labels_func(labels, preds, imp_indexes)
+    res = cal_metric(group_labels, group_preds, metrics)
+    return res['group_auc']
+
 def train(model,optimizer, args):
 
     print('params: '," T_warm: ",T_warm," all_iteration: ",all_iteration," lr: ",lr)
-    #writer = SummaryWriter('./model_snapshot_error')
-    # cuda_list=range(cuda_num)
     cuda_list=range(args.size)
-    #model.cuda(cudaid)
-    # accumulation_steps=40
-    accumulation_steps=int(args.batch_size/args.size/8)
-    #accumulation_steps=1
-    model = nn.DataParallel(model, device_ids=cuda_list)
+    accumulation_steps=int(args.batch_size/args.size/args.gpu_size)
+    #model = nn.DataParallel(model, device_ids=cuda_list)
+
+    # torch.cuda.set_device(cudaid)
+    # torch.distributed.init_process_group(backend='nccl', init_method='tcp://localhost:23456', rank=0, world_size=1)
+    # model=torch.nn.parallel.DistributedDataParallel(model, device_ids=cuda_list,output_device=0,find_unused_parameters=True)
+
+    model = torch.nn.DataParallel(model)
     accum_batch_loss=0
-    #train_file='train_ms_roberta_plain_pair_sample_shuffle.txt'
-    #train_file='train_ms_roberta_plain_pair_sample_large_new_shuffle.txt'
-    #train_file='train_ms_roberta.txt'
-    iterator=NewsIterator(batch_size=8*args.size, npratio=4,feature_file=os.path.join(args.data_dir,args.feature_file))
+    iterator=NewsIterator(batch_size=args.gpu_size*args.size, npratio=4,feature_file=os.path.join(args.data_dir,args.feature_file),field=args.field)
     train_file=os.path.join(args.data_dir, args.data_file)  
     #for epoch in range(0,100):
     batch_t=0
@@ -145,14 +208,19 @@ def train(model,optimizer, args):
     print('train...',cuda_list)
     #w=open(os.path.join(args.data_dir,args.log_file),'w')
     writer = SummaryWriter(os.path.join(args.data_dir, args.log_file) )
-    
     epoch=0
     model.train()
     # batch_t=52880-1
     # iteration=3305-1
     batch_t=0
     iteration=0
+    step=0
+    best_score=-1
     #w=open(os.path.join(args.data_dir,args.log_file),'w')
+
+    # model.eval()
+    # auc=test(model,args)
+
     for epoch in range(0,10):
     #while True:
         all_loss=0
@@ -160,27 +228,14 @@ def train(model,optimizer, args):
         data_batch=iterator.load_data_from_file(train_file)
         for  imp_index , user_index, his_id, candidate_id , label in data_batch:
             batch_t+=1
-            # if batch_t<=232240:
-            # if batch_t<=317190:
-            #   if (batch_t)%accumulation_steps==0:
-            #       iteration+=1
-            #   continue
-            
-
-            # print('shape: ',his_id.shape,candidate_id.shape,label.shape)
-            # print('candidate_id: ',candidate_id)
             assert candidate_id.shape[1]==2
             his_id=his_id.cuda(cudaid)
             candidate_id= candidate_id.cuda(cudaid)
             label = label.cuda(cudaid)
-            loss,sample_size=model(his_id,candidate_id, label)
+            loss=model(his_id,candidate_id, label)
 
-            sample_size=float(sample_size.sum())
+            sample_size=candidate_id.shape[0]
             loss=loss.sum()/sample_size/math.log(2)
-            # sample_size=float(sample_size)
-            # loss=loss/sample_size/math.log(2)
-            #print(' batch_t: ',batch_t, '  epoch: ',epoch,' loss: ',float(loss))
-            #print('???loss',loss)
             
             accum_batch_loss+=float(loss)
 
@@ -191,39 +246,28 @@ def train(model,optimizer, args):
             loss.backward()
 
             if (batch_t)%accumulation_steps==0:
-                #print('candidate_id: ',candidate_id)
-                # total_norm=0
-                # for p in model.parameters():
-                #     if p.grad==None:
-                #         print('error: ',index,p.size(),p.grad)
-                #     param_norm = p.grad.data.norm(2)
-                #     total_norm += param_norm.item() ** 2
-                # total_norm = total_norm ** (1. / 2)
-                #torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-
-                # total_clip_norm=0
-                # for p in model.parameters():
-                #     if p.grad==None:
-                #         print('error: ',index,p.size(),p.grad)
-                #     param_norm = p.grad.data.norm(2)
-                #     total_clip_norm += param_norm.item() ** 2
-                # total_clip_norm = total_clip_norm ** (1. / 2)
-
 
                 iteration+=1
                 adjust_learning_rate(optimizer,iteration)
-                
                 optimizer.step()
                 optimizer.zero_grad()
                 print(' batch_t: ',batch_t, ' iteration: ', iteration, ' epoch: ',epoch,' accum_batch_loss: ',accum_batch_loss/accumulation_steps,' lr: ', optimizer.param_groups[0]['lr'])
-                #w.write(' batch_t: '+str(batch_t)+' iteration: '+str(iteration)+' epoch: '+str(epoch)+' accum_batch_loss: '+str(accum_batch_loss/accumulation_steps)+'\n')
                 writer.add_scalar('Loss/train', accum_batch_loss/accumulation_steps, iteration)
+                writer.add_scalar('Ltr/train', optimizer.param_groups[0]['lr'], iteration)
                 accum_batch_loss=0
-                #assert epoch>=3
-                # torch.save(model.state_dict(),'./model/Plain_bert_960b_large'+str(epoch)+'.pkl')
-                
-                #writer.add_scalar('Loss/train', float(accum_batch_loss/accumulation_steps), iteration)
-                #break
+                if iteration%500==0:
+                    torch.cuda.empty_cache()
+                    model.eval()
+                    auc=test(model,args)
+                    print(auc)
+                    if auc>best_score:
+                        torch.save(model.state_dict(), os.path.join(args.save_dir,'Plain_robert_dot_best.pkl'))
+                        best_score=auc
+                        print('best score: ',best_score)
+                        writer.add_scalar('auc/valid', auc, step)
+                        step+=1
+                    torch.cuda.empty_cache()
+                    model.train()
         torch.save(model.state_dict(), os.path.join(args.save_dir,'Plain_robert_dot'+str(epoch)+'.pkl'))
     #w.close()
             
@@ -237,62 +281,34 @@ if __name__ == '__main__':
     torch.cuda.manual_seed(1)
     #main()
     args = parse_args()
-    mydict=utils.load_dict(os.path.join(args.data_dir,'roberta.base'))
-    model=Plain_bert(padding_idx=mydict['<pad>'],vocab_size=len(mydict))
-
+    model=Plain_bert(args)
     #optimizer = torch.optim.Adam(model.parameters(), lr=lr,betas=(0.9,0.98),eps=1e-6,weight_decay=0.0)
     optimizer = apex.optimizers.FusedLAMB(model.parameters(), lr=lr,betas=(0.9,0.98),eps=1e-6,weight_decay=0.0,max_grad_norm=1.0)
-
     
-    model_dict = model.state_dict()
-    # for k, v in model_dict.items(): 
-    #     print(k,v.size())
-    # print('----------------------------------------------------')
+    # for name, param in model.named_parameters():
+    #     print(name,param.shape,param.requires_grad)
 
     roberta = RobertaModel.from_pretrained(os.path.join(args.data_dir,'roberta.base'), checkpoint_file='model.pt')
-    # print('load model from Plain_bert_960b4...')
-    #save_model=torch.load('./model/Plain_bert_960b_large_continue_continue2.pkl',map_location=lambda storage, loc: storage)
-    # model_file=os.path.join(args.save_dir,'Plain_robert_dot0.pkl')
-    # save_model=torch.load(model_file, map_location=lambda storage, loc: storage)
+
+    # for name, param in roberta.named_parameters():
+    #     print(name,param.shape,param.requires_grad)
+
+
+    model_dict = model.state_dict()
     pretrained_dict={}
     for name,parameters in roberta.named_parameters():
-    #for name in save_model:
-        #print(name,':',save_model[name].size())
-        # print(name,':',parameters.size())
-        # if "layer_norm" in  name:
-        #   print(name, save_model[name], save_model[name].shape)
-        # if ( 'layers' in name ):
-        #   pretrained_dict[name[31:]]=parameters
-        # elif ('embed_positions.weight' in name or 'embed_tokens' in name or 'emb_layer_norm' in name):
-        #   pretrained_dict[name[31:]]=parameters
-
         if  'lm_head' not in name:
-            pretrained_dict[name[31:]]=parameters
+            pretrained_dict['encoder.'+name[31:]]=parameters
 
-
-        #pretrained_dict[name[7:]]=save_model[name]
-        #pretrained_dict[name]=save_model[name]
-        # elif 'lm_head.' in name:
-        #     pretrained_dict[name[14:]]=parameters
-    # print('----------------------------------------------------------')
     print(pretrained_dict.keys())
-    #assert 1==0
-
     model_dict.update(pretrained_dict)
-    #print(model_dict.keys())
     model.load_state_dict(model_dict)
+
     # for item in model.parameters():
     #   print(item.requires_grad)
-        
-    
     model.cuda(cudaid)
     train(model,optimizer,args)
-    # for epoch in range(5):
-    #   iteration=train(model,epoch,optimizer,iteration,cuda_num)
-    #   res=test(model)
-    #   print(res)
-        # optimizer.step()
-        # optimizer.zero_grad()
+    
 
             
 
