f53b177560425088769149d9d5d1b1cd677bb809
RUCer <lsqrucer@gmail.com>
2020-08-29 16:09:52 +0000

    plz

--- train_plain_bert_dot2.py
+++ train_plain_bert_dot2.py
@@ -141,7 +141,7 @@ def test(model,arges):
     labels = []
     imp_indexes = []
     feature_file=os.path.join(args.data_dir,args.feature_file)
-    iterator=NewsIterator(batch_size=1800, npratio=-1,feature_file=feature_file,field=args.field)
+    iterator=NewsIterator(batch_size=900, npratio=-1,feature_file=feature_file,field=args.field)
     print('test...')
     with torch.no_grad():
         data_batch=iterator.load_data_from_file(test_file)
@@ -191,6 +191,7 @@ def train(model,optimizer, args):
     model.train()
     # batch_t=52880-1
     # iteration=3305-1
+    best_score=-1
     batch_t=0
     iteration=0
     #w=open(os.path.join(args.data_dir,args.log_file),'w')
