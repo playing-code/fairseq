2105ef122cc469b1f0ad9a27cb9e10947a27f504
RUCer <lsqrucer@gmail.com>
2020-09-12 13:44:19 +0000

    **

--- train_sparse_bert_dot4_fp16_hf.py
+++ train_sparse_bert_dot4_fp16_hf.py
@@ -273,8 +273,11 @@ def train(cudaid, args,model):
             all_loss+=float(loss)
             all_batch+=1
 
-            # if cudaid==1:
-                
+            if cudaid==1:
+                handle = pynvml.nvmlDeviceGetHandleByIndex(cudaid)
+                meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
+                #print(int(meminfo.used)/1024/1024)
+                print('loss: ',loss,int(meminfo.used)/1024/1024)
                 # torch.set_printoptions(profile="full")
                 # w=open('input.txt','w')
                 # w.write(str(his_id.cpu()))
@@ -294,10 +297,6 @@ def train(cudaid, args,model):
                 optimizer.step()
                 optimizer.zero_grad()
                 if cudaid==0:
-                    # handle = pynvml.nvmlDeviceGetHandleByIndex(cudaid)
-                    # meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
-                    # #print(int(meminfo.used)/1024/1024)
-                    # print('loss: ',loss,int(meminfo.used)/1024/1024)
                     print(' batch_t: ',batch_t, ' iteration: ', iteration, ' epoch: ',epoch,' accum_batch_loss: ',accum_batch_loss/accumulation_steps,' lr: ', optimizer.param_groups[0]['lr'])
                     writer.add_scalar('Loss/train', accum_batch_loss/accumulation_steps, iteration)
                     writer.add_scalar('Ltr/train', optimizer.param_groups[0]['lr'], iteration)
